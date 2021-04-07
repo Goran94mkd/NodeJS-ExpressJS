6c606a1116a491b2fec4314f986b9413ae396ac6
Goran94mkd <goki_bituse@hotmail.com>
2021-04-07 02:25:28 +0200

    Add controllers pattern

--- Healthcare-App/models/patient.js
+++ Healthcare-App/models/patient.js
@@ -25,11 +25,11 @@ const patientSchema = mongoose.Schema({
   phone_number: {
     type: String,
     required: ['Phone number is a required field'],
-    unique: true,
-    validate: {
-      validator: value => /^[0]{1}[7]{1}[0, 1, 2, 5, 6, 7, 8]{1}[0-9]{6}$/.test(value),
-      message: props => `The phone number ${props.value} is invalid`
-    }
+    // unique: true,
+    // validate: {
+    //   validator: value => /^[0]{1}[7]{1}[0, 1, 2, 5, 6, 7, 8]{1}[0-9]{6}$/.test(value),
+    //   message: props => `The phone number ${props.value} is invalid`
+    // }
   },
   city: {
     type: String,
